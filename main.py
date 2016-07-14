0091c0208536052438fde794934e87f4a9713049
QUANON <quanon86@gmail.com>
2016-07-15 00:51:21 +0900

    added Post()

--- main.py
+++ main.py
@@ -4,80 +4,111 @@ import urllib
 import requests
 import csv
 
+class Post():
+    def __init__(self, insta_id, max_id):
+        self.insta_id = insta_id
+        self.max_id = max_id
+        self.__url = None
+        self.__json_response = None
+        self.__items = []
+
+    @property
+    def url(self):
+        if self.__url:
+            return self.__url
+
+        self.__url = os.path.join('https://www.instagram.com', self.insta_id, 'media')
+
+        if self.max_id:
+            self.__url += ('?max_id=' + self.max_id)
+
+        return self.__url
+
+    @property
+    def json_response(self):
+        if self.__json_response:
+            return self.__json_response
+
+        response = requests.get(self.url)
+        self.__json_response = response.json()
+
+        return self.__json_response
+
+    @property
+    def items(self):
+        if self.__items:
+            return self.__items
+
+        self.__items = self.json_response['items']
+
+        return self.__items
+
+    def more_available(self):
+        return self.json_response['more_available'] == True
+
 class PostList():
     def __init__(self, insta_id):
         self.insta_id = insta_id
-        self.posts = []
+        self.items = []
 
     def to_list(self):
-        if self.posts:
-            return self.posts
+        if self.items:
+            return self.items
 
         max_id = None
 
         while True:
-            json = self.__get_json_response(max_id)
+            post = Post(insta_id, max_id)
 
-            for i in json['items']:
-                self.posts.append(i)
+            for item in post.items:
+                self.items.append(item)
 
-            if json['more_available'] is not True:
+            if post.more_available() is not True:
                 break
 
-            max_id = json['items'][-1]['id']
+            max_id = post.items[-1]['id']
 
-        return self.posts
-
-    def __get_json_response(self, max_id):
-        url =  self.__build_url(max_id)
-        response = requests.get(url)
-        json = response.json()
-
-        return json
-
-    def __build_url(self, max_id):
-        url = os.path.join('https://www.instagram.com', self.insta_id, 'media')
-
-        if max_id:
-            url += ('?max_id=' + max_id)
-
-        return url
+        return self.items
 
 class Account():
     def __init__(self, insta_id):
         self.insta_id = insta_id
-        self.post_list = []
-        self.stats_list = []
-        self.caption_list = []
+        self.__post_list = []
+        self.__stats_list = []
+        self.__caption_list = []
+        self.__bio = None
 
-    def get_post_list(self):
-        if self.post_list:
-            return self.post_list
+    @property
+    def post_list(self):
+        if self.__post_list:
+            return self.__post_list
 
-        self.post_list = PostList(insta_id).to_list()
+        self.__post_list = PostList(insta_id).to_list()
 
-        return self.post_list
+        return self.__post_list
 
-    def get_stats_list(self):
-        if self.stats_list:
-            return self.stats_list
+    @property
+    def stats_list(self):
+        if self.__stats_list:
+            return self.__stats_list
 
-        post_list = self.get_post_list()
+        post_list = self.post_list
 
         for post in post_list:
-            self.stats_list.append([
+            self.__stats_list.append([
                 post['code'],
                 post['likes']['count'],
                 post['comments']['count']
             ])
 
-        return self.stats_list
+        return self.__stats_list
 
-    def get_caption_list(self):
-        if self.caption_list:
-            return self.caption_list
+    @property
+    def caption_list(self):
+        if self.__caption_list:
+            return self.__caption_list
 
-        post_list = self.get_post_list()
+        post_list = self.post_list
 
         for post in post_list:
             caption = [post['code']]
@@ -85,9 +116,23 @@ class Account():
             if post['caption']:
                 caption.append(post['caption']['text'].replace('\n', ' '))
 
-            self.caption_list.append(caption)
+            self.__caption_list.append(caption)
 
-        return self.caption_list
+        return self.__caption_list
+
+    @property
+    def bio(self):
+        if self.__bio:
+            return self.__bio
+
+        response = requests.get('https://www.instagram.com/' + self.insta_id)
+        page_source = response.text
+        import pdb; pdb.set_trace()
+        start_index = page_source.find("<meta property=\"og:description\" content=") + len("<meta property=\"og:description\" content=")
+        end_index = page_source.find("/>\n",start_index)
+        self.__bio = page_source[start_index:end_index]
+
+        return self.__bio
 
 def download_photos(post_list):
     '''
@@ -119,29 +164,24 @@ def write_to_csv(list_name,file_name,col_names):
 
     output_file.close()
 
-def write_bio(insta_id):
+def write_bio(account):
     '''
-    INTAKE: an Instagram id
+    INTAKE: an Account object
     RETURN: a csv file with bio
     '''
-    r = requests.get("https://www.instagram.com/" + insta_id)
-    page_source = r.text
-
-    start_index = page_source.find("<meta property=\"og:description\" content=") + len("<meta property=\"og:description\" content=")
-    end_index = page_source.find("/>\n",start_index)
-    bio = page_source[start_index:end_index]
+    bio = account.bio
 
     bio_file = open(insta_id + '_bio.csv','w')
     bio_writer = csv.writer(bio_file)
     bio_writer.writerow(['instagram_id','bio'])
-    bio_writer.writerow([insta_id,bio])
+    bio_writer.writerow([insta_id, bio])
 
 if __name__ == '__main__':
     insta_id = sys.argv[1]
 
     account = Account(insta_id)
 
-    write_to_csv(account.get_stats_list(), 'stats_list', ['post_id','num_of_likes','num_of_comments'])
-    write_to_csv(account.get_caption_list(), 'caption_list', ['post_id','caption'])
+    write_to_csv(account.stats_list, 'stats_list', ['post_id','num_of_likes','num_of_comments'])
+    write_to_csv(account.caption_list, 'caption_list', ['post_id','caption'])
     # download_photos(post_list)
-    write_bio(insta_id)
+    write_bio(account)
